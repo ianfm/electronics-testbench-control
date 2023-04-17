3ec89e3dc3b87b42667bba44caf93069de967154
Ian McMurray <ian@farmx.co>
2023-04-16 19:06:25 -0700

    Add wait times between calls to test script

--- test/spd1305-args-test.ps1
+++ test/spd1305-args-test.ps1
@@ -1,11 +1,14 @@
 # Argparse command test
-#TODO need pauses between commands
 
 # Test channel, voltage, and current setters
 python ..\siglent_SPD1305X.py --set-channel 1 --set-voltage 3.0 --set-current 0.003
+sleep(1)
 python ..\siglent_SPD1305X.py --set-channel 1 --set-voltage 3.3 --set-current 0.010
+sleep(1)
 
 # Test display feature
 python ..\siglent_SPD1305X.py --display-on
+sleep(1)
 python ..\siglent_SPD1305X.py --display-off
+
 
